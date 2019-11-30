40d0613e6bbd238ce415522b19141aec8f3b8080
Stephen <stephen.utama@mail.utoronto.ca>
2019-11-30 14:28:36 -0500

    connected checkout component to backend

--- express_server/routes/orders.js
+++ express_server/routes/orders.js
@@ -4,66 +4,84 @@ const { Order } = require("../schemas/order");
 const uuidv4 = require("uuid/v4");
 
 router.get("", (req, res) => {
-    Order.find({userId: req.header("userId")}).exec().then(orders => {
-        res.status(200).json(orders);
+  Order.find({ userId: req.header("userId") })
+    .exec()
+    .then(orders => {
+      res.status(200).json(orders);
     });
 });
 
 router.get("/:orderId", (req, res) => {
-    Order.findOne({id: req.params.orderId, userId: req.header("userId")}).exec().then(order => {
-        if (order) {
-            res.status(200).json(order);
-        } else {
-            res.status(404).json({
-                message: `Order ${req.params.orderId} does not exist for user ${req.header("userId")}`
-            });
-        }
-    })
+  Order.findOne({ id: req.params.orderId, userId: req.header("userId") })
+    .exec()
+    .then(order => {
+      if (order) {
+        res.status(200).json(order);
+      } else {
+        res.status(404).json({
+          message: `Order ${
+            req.params.orderId
+          } does not exist for user ${req.header("userId")}`
+        });
+      }
+    });
 });
 
 router.post("", (req, res) => {
-    let newId = uuidv4();
-    Order.findOne({id: newId}).exec().then(doc => {
-        if (!doc) {
-            let newOrder = new Order({
-                id: newId,
-                userId: req.header("userId"),
-                items: req.body.items,
-            });
-            newOrder.save().then(order => {
-                    res.status(201).json({...(order._doc),...{href: req.url + `/api/orders/${newId}`}})
-                }
-            )
-        } else {
-            res.status(400).json({
-                message: `Bad request, order ${newId} already exists.`
-            })
-        }
-    })
+  let newId = uuidv4();
+  Order.findOne({ id: newId })
+    .exec()
+    .then(doc => {
+      if (!doc) {
+        let newOrder = new Order({
+          id: newId,
+          userId: req.session.user,
+          //   userId: req.header("userId"),
+          items: req.body.items
+        });
+        newOrder.save().then(order => {
+          res.status(201).json({
+            ...order._doc,
+            ...{ href: req.url + `/api/orders/${newId}` }
+          });
+        });
+      } else {
+        res.status(400).json({
+          message: `Bad request, order ${newId} already exists.`
+        });
+      }
+    });
 });
 
-router.patch("/:orderId", (req,res) => {
-    let query = {
-        id: req.params.orderId,
-        userId: req.header("userId")
-    };
-    let updates = {
-        items: req.body.items,
-        lastUpdated: Date.now()
-    };
-    Order.findOne(query).exec().then(order => {
-        if(order) {
-            Order.findOneAndUpdate(query, updates, {new: true}).exec().then(updatedOrder => {
-                res.status(202).json(updatedOrder)
-            });
-        } else {
-            res.status(404).json({
-                message: `Order ${req.params.orderId} not found for user ${req.header("userId")}`
-            })
-        }
-    }).catch(err => {
-        res.status(500).json(err)
+router.patch("/:orderId", (req, res) => {
+  let query = {
+    id: req.params.orderId,
+    userId: req.header("userId")
+  };
+  let updates = {
+    items: req.body.items,
+    lastUpdated: Date.now()
+  };
+  Order.findOne(query)
+    .exec()
+    .then(order => {
+      if (order) {
+        Order.findOneAndUpdate(query, updates, { new: true })
+          .exec()
+          .then(updatedOrder => {
+            res.status(202).json(updatedOrder);
+          });
+      } else {
+        res.status(404).json({
+          message: `Order ${req.params.orderId} not found for user ${req.header(
+            "userId"
+          )}`
+        });
+      }
     })
+    .catch(err => {
+      res.status(500).json(err);
+    });
 });
 
 module.exports = router;
--- react_app/src/components/CheckoutComponent/CheckoutComponent.js
+++ react_app/src/components/CheckoutComponent/CheckoutComponent.js
@@ -1,96 +1,137 @@
-import React, {Component, Fragment} from "react";
-import {Card, CardContent, Typography} from "@material-ui/core";
-import {connect} from "react-redux";
-import "./CheckoutComponent.css"
+import React, { Component, Fragment } from "react";
+import { Card, CardContent, Typography } from "@material-ui/core";
+import { connect } from "react-redux";
+import "./CheckoutComponent.css";
 import Divider from "@material-ui/core/Divider";
 import Button from "@material-ui/core/Button";
-import {CLEAR_CART} from "../../redux/actions";
+import { CLEAR_CART } from "../../redux/actions";
 
 const mapStateToProps = state => {
-    return {
-        cart : state.cart
-    }
+  return {
+    cart: state.cart
+  };
 };
 
 class CheckoutComponent extends Component {
-    constructor(){
-        super();
-        this.totalCost = this.totalCost.bind(this);
-        this.thankYouRedirect = this.thankYouRedirect.bind(this);
-        this.cartDisplayState = this.cartDisplayState.bind(this);
-    }
+  constructor() {
+    super();
+    this.totalCost = this.totalCost.bind(this);
+    this.thankYouRedirect = this.thankYouRedirect.bind(this);
+    this.cartDisplayState = this.cartDisplayState.bind(this);
+  }
 
-    totalCost() {
-        return this.props.cart.reduce((a,b) => a + (b['price']*b['count'] || 0), 0)
-    }
+  totalCost() {
+    return this.props.cart.reduce(
+      (a, b) => a + (b["price"] * b["count"] || 0),
+      0
+    );
+  }
 
-    thankYouRedirect() {
-        this.props.dispatch(CLEAR_CART());
-        alert("Thanks for shopping with us!");
-        this.props.history.push('/');
-    }
+  thankYouRedirect() {
+    this.props.dispatch(CLEAR_CART());
+    alert("Thanks for shopping with us!");
+    this.props.history.push("/");
+  }
 
-    cartDisplayState() {
-        return this.props.cart.length <= 0? "none" : "flex";
-    }
+  cartDisplayState() {
+    return this.props.cart.length <= 0 ? "none" : "flex";
+  }
 
-    render() {
-        console.log(this.props.cart)
+  handleCheckOut() {
+    let array = [];
+    this.props.cart.map(item => {
+      array.push({
+        quantity: item.count,
+        currentUnitPrice: item.price,
+        itemName: item.name
+      });
+    });
+    fetch("http://localhost:3001/api/orders", {
+      method: "post",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+      credentials: "include",
+      body: JSON.stringify({
+        items: array
+      })
+    }).then(res => {
+      if (res.status === 201) {
+        this.thankYouRedirect();
+      }
+    });
+  }
 
-        const checkoutButtons = {
-                'font-family': '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
-                'font-size': 'small',
-                'padding-left' :'0',
-                'padding-right' : '2',
-                'justify-content' : 'left',
-                'min-width':'0',
-                'text-transform' : 'capitalize'
-        };
+  render() {
+    console.log(this.props.cart);
 
-        return (
-            <div>
-                {this.props.cart.map(item => {
-                    return(
-                        <Fragment>
-                            <Card>
-                                <img className='checkoutImages' src={item.imageUrls} alt="eet"/>
-                                <CardContent component='span'>
-                                    <Typography variant='h5'>
-                                        {item.name}
-                                    </Typography>
-                                    <span >
-                                        Quantity: {item.count}
-                                    </span>
-                                    <span className='checkoutPricing'>
-                                        ${item.count * item.price}
-                                    </span>
-                                    <div>
-                                        <Button style={checkoutButtons}>Add</Button>
-                                        <Button style={checkoutButtons}>Remove</Button>
-                                        <Button style={checkoutButtons}>Delete</Button>
-                                    </div>
-                                </CardContent>
-                            </Card>
-                            <Divider/>
-                        </Fragment>
-                    );})}
+    const checkoutButtons = {
+      "font-family":
+        '-apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto", "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans", "Helvetica Neue", sans-serif',
+      "font-size": "small",
+      "padding-left": "0",
+      "padding-right": "2",
+      "justify-content": "left",
+      "min-width": "0",
+      "text-transform": "capitalize"
+    };
 
-                <div style={{display: this.cartDisplayState()}} className='checkoutPricing'>
-                    <Typography variant={"subtitle1"}>
-                        Total: ${this.totalCost()}
-                    </Typography>
-                </div>
-                <div style={{display: this.cartDisplayState(), clear:'right'}} className='checkoutButton'>
+    return (
+      <div>
+        {this.props.cart.map(item => {
+          return (
+            <Fragment>
+              <Card>
+                <img
+                  className="checkoutImages"
+                  src={item.imageUrls}
+                  alt="eet"
+                />
+                <CardContent component="span">
+                  <Typography variant="h5">{item.name}</Typography>
+                  <span>Quantity: {item.count}</span>
+                  <span className="checkoutPricing">
+                    ${item.count * item.price}
+                  </span>
+                  <div>
+                    <Button style={checkoutButtons}>Add</Button>
+                    <Button style={checkoutButtons}>Remove</Button>
+                    <Button style={checkoutButtons}>Delete</Button>
+                  </div>
+                </CardContent>
+              </Card>
+              <Divider />
+            </Fragment>
+          );
+        })}
 
-                    <Button variant='contained' color='primary' onClick={this.thankYouRedirect}>
-                        Buy It
-                    </Button>
-                </div>
-            </div>
-
-
-        )
-    }
+        <div
+          style={{ display: this.cartDisplayState() }}
+          className="checkoutPricing"
+        >
+          <Typography variant={"subtitle1"}>
+            Total: ${this.totalCost()}
+          </Typography>
+        </div>
+        <div
+          style={{ display: this.cartDisplayState(), clear: "right" }}
+          className="checkoutButton"
+        >
+          <Button
+            variant="contained"
+            color="primary"
+            onClick={() => {
+              this.handleCheckOut();
+              //   this.thankYouRedirect;
+            }}
+          >
+            Buy It
+          </Button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default connect(mapStateToProps)(CheckoutComponent);
