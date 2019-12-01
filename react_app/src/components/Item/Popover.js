93961c98e11269158d63215fe9de82475efe4ae6
Lingjing <44420626+Lingjingzou@users.noreply.github.com>
2019-12-01 18:30:37 -0500

    Add a popover modal

---
+++ react_app/src/components/Item/Popover.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Popover from '@material-ui/core/Popover';
+import Typography from '@material-ui/core/Typography';
+
+const useStyles = makeStyles(theme => ({
+  typography: {
+    padding: theme.spacing(2),
+  },
+}));
+
+export default function SimplePopover() {
+  const classes = useStyles();
+  const [anchorEl, setAnchorEl] = React.useState(null);
+
+  const handleClick = event => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const open = Boolean(anchorEl);
+  const id = open ? 'simple-popover' : undefined;
+
+  return (
+    <div>
+      <div onClick={handleClick}>
+        Details
+      </div>
+      <Popover id=""
+        id={id}
+        open={open}
+        anchorEl={anchorEl}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: 'bottom',
+          horizontal: 'center',
+        }}
+        transformOrigin={{
+          vertical: 'top',
+          horizontal: 'center',
+        }}
+      >
+        <Typography id="description" className={classes.typography}>"Product #: 5240376 Nike's Air Max line shook up the running world in 1997 when it debuted full-length Max Air cushioning."</Typography>
+      </Popover>
+    </div>
+  );
+}
