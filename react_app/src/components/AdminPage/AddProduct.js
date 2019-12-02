c9b0c20fd1ceeef5b1e59bf1db833fbd7aae84f3
Stephen <stephen.utama@mail.utoronto.ca>
2019-12-01 21:36:40 -0500

    merging

--- react_app/src/components/AdminPage/AddProduct.js
+++ react_app/src/components/AdminPage/AddProduct.js
@@ -1,68 +1,82 @@
 import React, { Component } from "react";
 import Button from "@material-ui/core/Button";
 import Container from "@material-ui/core/Container";
-import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
+import Typography from "@material-ui/core/Typography";
+import TextField from "@material-ui/core/TextField";
 // import { connect } from "react-redux";
 // import Item from "../Item/Item";
 
-import SimplePopover from './Popover.js';
+// import SimplePopover from './Popover.js';
 
 class AddProduct extends Component {
+  constructor(props) {
+    super(props);
 
-    constructor(props) {
-        super(props);
-    
-        this.state = {
-            //get item and set state
-        };
-      }
-    render() {
-      return (
-        <Container component="main">
+    this.state = {
+      //get item and set state
+    };
+  }
+  render() {
+    return (
+      <Container component="main">
         <div style={{ padding: 30 }}>
-        <Typography variant="h2" gutterBottom>
-        Add More Products!
-       </Typography>
-       </div>
+          <Typography variant="h2" gutterBottom>
+            Add More Products!
+          </Typography>
+        </div>
 
-       <div style={{ padding: 10 }}>
-       <div
+        <div style={{ padding: 10 }}>
+          <div
             style={{
               flex: 1,
               marginLeft: 20,
               display: "flex",
               flexDirection: "column"
-            }} >
+            }}
+          >
+            <TextField
+              style={{ width: 300, marginTop: 15, height: 50 }}
+              id="standard-basic"
+              label="Name"
+            />
 
-      <TextField style={{ width: 300, marginTop: 15 , height: 50}} 
-      id="standard-basic" label="Name" />
+            <TextField
+              style={{ width: 300, marginTop: 15, height: 50 }}
+              id="standard-basic"
+              label="Category"
+            />
 
-     <TextField style={{ width: 300, marginTop: 15 , height: 50}} 
-      id="standard-basic" label="Category" />
+            <TextField
+              style={{ width: 150, marginTop: 15, height: 50 }}
+              id="standard-basic"
+              label="Price"
+            />
 
+            <TextField
+              style={{ width: 500, marginTop: 15, height: 50 }}
+              id="standard-basic"
+              label="Descriptions"
+            />
 
-     <TextField style={{ width: 150, marginTop: 15 , height: 50}} 
-      id="standard-basic" label="Price" />   
+            <TextField
+              style={{ width: 500, marginTop: 15, height: 50 }}
+              id="standard-basic"
+              label="Image URL"
+            />
 
-     <TextField style={{ width: 500, marginTop: 15 , height: 50}} 
-      id="standard-basic" label="Descriptions" /> 
-    
-     <TextField style={{ width: 500, marginTop: 15 , height: 50}} 
-      id="standard-basic" label="Image URL" /> 
+            <Button
+              style={{ width: 200, marginTop: 15, height: 50 }}
+              variant="contained"
+              color="primary"
+            >
+              Add Product
+            </Button>
 
-       <Button style={{ width: 200, marginTop: 15 , height: 50}} variant="contained" color="primary">
-        Add Product
-       </Button>
-
-       <SimplePopover>
-
-       </SimplePopover>
-       </div>
-       </div>
-        </Container>
-        
-      );
-    }
+            {/* <SimplePopover></SimplePopover> */}
+          </div>
+        </div>
+      </Container>
+    );
   }
+}
 export default AddProduct;
